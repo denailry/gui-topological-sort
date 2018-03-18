b49182acd629bcad609988fe36dd90fb5f84e772
Erma Safira Nurmasyita <erma.safira@gmail.com>
2018-03-18 19:55:03 +0700

    edit DFS

--- topological-sort/TopologicalSort.cs
+++ topological-sort/TopologicalSort.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace topological_sort
 {
     public class TopologicalSort
     {
-        Graph graph;
-        List<string> result;
+        private Graph graph;
+        private List<string> result;
 
         public TopologicalSort()
         {
@@ -21,46 +22,58 @@ namespace topological_sort
 
         }
 
-        public void DFSUtil(Graph.Vertex V, ref int timestamp, ref List<int> startstamp, ref List<int> stopstamp, ref List<bool> visited)
+        public void DFSUtil(Graph.Vertex V, ref int timestamp, ref List<int> startstamp, ref List<int> stopstamp, ref List<bool> visited, ref StreamWriter sw)
         {
             int i;
             foreach (string vertexname in graph.GetNeighbor(V.data)) {
                 i = graph.GetVertexIndex(vertexname);
-                if (!visited[i]) {
-                    timestamp++;
+                timestamp++;
+                if (!visited[i])
+                {
                     startstamp[i] = timestamp;
-                    DFSUtil(graph.GetVertex(i), ref timestamp, ref startstamp, ref stopstamp, ref visited);
+                    sw.WriteLine();
+                    DFSUtil(graph.GetVertex(i), ref timestamp, ref startstamp, ref stopstamp, ref visited, ref sw);
                     stopstamp[i] = timestamp;
                 }
+                else
+                    sw.WriteLine("<");
             }
         }
 
         public void DFS()
         {
+            StreamWriter sw = new StreamWriter("DFS.txt");
             int timestamp = 0;
             int i;
             List<bool> visited = new List<bool>(new bool[graph.GetGraphSize()]);
             List<int> startstamp = new List<int>(graph.GetGraphSize());
             List<int> stopstamp = new List<int>(graph.GetGraphSize());
+            List<string> res = new List<string>(graph.GetGraphSize());
 
             foreach (Graph.Vertex V in graph.GetVertices())
             {
+                timestamp++;
                 i = V.GetIndex();
                 if (!visited[i]) {
-                    timestamp++;
                     startstamp[i] = timestamp;
-                    DFSUtil(V, ref timestamp, ref startstamp, ref stopstamp, ref visited);
+                    sw.WriteLine(V.data);
+                    DFSUtil(V, ref timestamp, ref startstamp, ref stopstamp, ref visited, ref sw);
+                    sw.WriteLine("<");
                     stopstamp[i] = timestamp;
                 }
             }
-            /*//Sort based on stopstamp
+            sw.Close();
+            //Sort based on stopstamp
             var sorted = stopstamp
                 .Select((x, j) => new KeyValuePair<int, int>(x, j))
-                .OrderBy(x < x.Key)
+                .OrderBy(x => x.Key)
                 .ToList();
-            List<int> result = sorted.Select(x => x.Key).ToList();
-            List<int> idx = sorted.Select(x => x.Value).ToList();
-            return */
+            //List<int> result = sorted.Select(x => x.Key).ToList();
+            List<int> resultIndex = sorted.Select(x => x.Value).ToList();
+            for (i = resultIndex.Capacity-1; i >= 0; i--) {
+                res[i] = graph.GetVertex(i).data;
+            }
+            result = res;
         }
 
         public List<string> GetResult()
