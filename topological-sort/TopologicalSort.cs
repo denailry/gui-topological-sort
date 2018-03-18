5415876de2ecc589d32078ba35a937fa610ac0fc
Daniel Ryan Levyspn <daniel.ryan@students.itb.ac.id>
2018-03-18 16:57:39 +0700

    Base

---
+++ topological-sort/TopologicalSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace topological_sort
+{
+    public class TopologicalSort
+    {
+        Graph graph;
+        List<string> result;
+
+        public TopologicalSort()
+        {
+
+        }
+
+        public void BFS()
+        {
+
+        }
+
+        public void DFS()
+        {
+
+        }
+
+        public List<string> GetResult()
+        {
+            return this.result;
+        }
+    }
+}
