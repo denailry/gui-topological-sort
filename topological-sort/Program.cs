a8150d8b749339a408a296989904853ce8c485e6
samuelsandi <samuelsandi1703@gmail.com>
2018-03-19 20:01:47 +0700

    ListBox5

--- topological-sort/Program.cs
+++ topological-sort/Program.cs
@@ -17,63 +17,6 @@ namespace topological_sort
             Form1 form1 = new Form1();
             Application.EnableVisualStyles();
             Application.Run(form1);
-<<<<<<< HEAD
-=======
-
-            Graph graph = new Graph(5);
-            graph.AddVertex("C1");
-            graph.AddVertex("C2");
-            graph.AddVertex("C3");
-            graph.AddVertex("C4");
-            graph.AddVertex("C5");
-
-            graph.AddEdge("C1", "C2");
-            graph.AddEdge("C2", "C3");
-            graph.AddEdge("C2", "C4");
-            graph.AddEdge("C1", "C4");
-            graph.AddEdge("C3", "C5");
-            graph.AddEdge("C4", "C5");
-            graph.AddEdge("C4", "C3");
-
-            TopologicalSort ts = new TopologicalSort(graph);
-            ts.BFS();
-            ts.DFS();
-            /*
-            //create a form 
-            System.Windows.Forms.Form form = new System.Windows.Forms.Form();
-            //create a viewer object 
-            Microsoft.Msagl.GraphViewerGdi.GViewer viewer = new Microsoft.Msagl.GraphViewerGdi.GViewer();
-            //create a graph object 
-            Microsoft.Msagl.Drawing.Graph graph = new Microsoft.Msagl.Drawing.Graph("graph");
-            //create the graph content 
-            graph.AddEdge("A", "B");
-            graph.AddEdge("B", "C");
-            graph.AddEdge("A", "C").Attr.Color = Microsoft.Msagl.Drawing.Color.Green;
-            graph.FindNode("A").Attr.FillColor = Microsoft.Msagl.Drawing.Color.Magenta;
-            graph.FindNode("B").Attr.FillColor = Microsoft.Msagl.Drawing.Color.MistyRose;
-            Microsoft.Msagl.Drawing.Node c = graph.FindNode("C");
-            c.Attr.FillColor = Microsoft.Msagl.Drawing.Color.PaleGreen;
-            c.Attr.Shape = Microsoft.Msagl.Drawing.Shape.Diamond;
-            //bind the graph to the viewer 
-            viewer.Graph = graph;
-            //associate the viewer with the form 
-            form.SuspendLayout();
-            viewer.Dock = System.Windows.Forms.DockStyle.Fill;
-            form.Controls.Add(viewer);
-            form.ResumeLayout();
-            //show the form 
-            form.ShowDialog();
-            graph.AddEdge("A", "C").Attr.Color = Microsoft.Msagl.Drawing.Color.Magenta;
-            form.Controls.Add(viewer);
-            form.ResumeLayout();
-            //show the form 
-            form.ShowDialog();
-            */
-
-            //Application.EnableVisualStyles();
-            //Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());
->>>>>>> d2aa0190bf48df751b34e2221318e82f864eb0c6
         }
     }
 }
